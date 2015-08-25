b5b9666bf29f5b65d6a83e933ff6ecba15c77822
fushang318 <fushang318@gmail.com>
2015-08-25 12:54:43 +0800

    merge #11

--- app/controllers/question_bank/questions_controller.rb
+++ app/controllers/question_bank/questions_controller.rb
@@ -1,12 +1,22 @@
 module QuestionBank
   class QuestionsController < QuestionBank::ApplicationController
-    def new
+    def new_bool
       @question = Question.new
     end
 
     def create
+      case params[:question][:kind]
+      when 'bool' then _create_bool
+      end
+    end
+
+    def _create_bool
       @question = Question.new(question_bool_params)
-      
+      if @question.save
+        redirect_to "/questions"
+      else
+        redirect_to "/questions/new_bool"
+      end
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 QuestionBank::Engine.routes.draw do
   # root 'home#index'
-  resources :questions
+  resources :questions do
+    collection do
+      get :new_bool
+    end
+  end
 end
