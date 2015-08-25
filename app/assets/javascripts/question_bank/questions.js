b18b6e9d8e03a224be58c05c354c633c481008a8
hengtangan2025 <504371515@qq.com>
2015-08-25 10:30:42 +0800

    825 1030

--- app/assets/javascripts/question_bank/questions.js
+++ app/assets/javascripts/question_bank/questions.js
@@ -1,14 +1,9 @@
 jQuery(document).ready(function(){
   var n = 1
-  jQuery('#add_choice').click(function(){
+  jQuery('.new-single-choice .add-choice').click(function(){
     n++
-    var m = n + 1 
-    html =
-      "<div class='radio_inline'>" + 
-        "<input type = 'radio' value = "+ n +" name = 'question[choice_answer_indexs][]' id = 'question_choice_answer_indexs_"+ n +"'>" +
-        "&nbsp"+
-        "<input type = 'text' name = 'question[choices][]' id = 'question_choices'>" +
-      "</div>" 
-    jQuery('#add_choice').before(html);
+    dom = jQuery('.new-single-choice .radio_inline:last').clone()
+    dom.find('#question_choice_answer_indexs_2').val(n)
+    jQuery('.new-single-choice .add-choice').before(dom);
   })
 })
