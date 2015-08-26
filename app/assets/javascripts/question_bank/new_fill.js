f740c5ad77d25fe580129c51602a803a0d44356a
XAINI <shanzhenqiang0814@163.com>
2015-08-26 10:33:46 +0800

    Finished the fill's adjustment

--- app/assets/javascripts/question_bank/new_fill.js
+++ app/assets/javascripts/question_bank/new_fill.js
@@ -1,18 +1,20 @@
-
-  jQuery(document).on('click','.page-new-question-fill .insert',function(){
-  var str = jQuery('.page-new-question-fill .input-filed').find('textarea').val();
+jQuery(document).on('click','.page-new-question-fill .insert',function(){
+  var str = jQuery('.page-new-question-fill').find("[name='question[content]']").val();
   str = str +" ___ "
-  jQuery('.page-new-question-fill .input-filed').find('textarea').val(str);
+  jQuery('.page-new-question-fill .question-content ').val(str);
+})
 
+jQuery(document).on('click','.page-new-question-fill .delete ',function(){
+  jQuery(this).parent().remove()
+})
 
+jQuery(document).on('click','.page-new-question-fill .append ',function(){ 
   var blank = jQuery('.page-new-question-fill .answer:last').clone();
   jQuery('.page-new-question-fill .answer:last').after(blank)
   jQuery('.page-new-question-fill .answer:last .hidden').removeClass('hidden')
   jQuery('.page-new-question-fill .answer:last input').val("")
+})
+
+jQuery(document).on('click','.page-new-question-fill .submit input',function(){
   jQuery('.page-new-question-fill .hidden').remove()
-  })
-
-  jQuery(document).on('click','.page-new-question-fill .delete',function(){
-   jQuery(this).closest('.answer').remove()
-  })
-
+})
