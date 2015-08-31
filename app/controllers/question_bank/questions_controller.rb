fb520e381d4b9e1c2cb00d7a47ff63d813e22866
XAINI <shanzhenqiang0814@163.com>
2015-08-31 13:55:47 +0800

    Finished the delete function

--- app/controllers/question_bank/questions_controller.rb
+++ app/controllers/question_bank/questions_controller.rb
@@ -56,6 +56,12 @@ module QuestionBank
       end
     end
 
+    def destroy
+      @question = Question.find(params[:id])
+      @question.destroy
+      redirect_to "/questions"
+    end
+
     private
       def question_bool_params
         params.require(:question).permit(:kind, :content, :bool_answer, :analysis, :level, :enabled)
