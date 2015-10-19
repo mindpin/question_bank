5c9d5a554e8e93012ea71297789ec81d7734d4aa
XAINI <shanzhenqiang0814@163.com>
2015-10-19 18:39:49 +0800

    complete 60%

--- app/models/question_bank/question_record.rb
+++ app/models/question_bank/question_record.rb
@@ -4,14 +4,19 @@ module QuestionBank
     include Mongoid::Timestamps
     
     field :is_correct, type: Boolean
-    field :bool_answer, type: Boolean
-    field :choice_answer, type: Array
-    field :essay_answer, type: String
-    field :fill_answer, type: Array
-    field :mapping_answer, type: Array
+    field :bool_answer, type: Boolean  # 判断题
+    field :choice_answer, type: Array  # 选择题(单选和多选)
+    field :essay_answer, type: String  # 论述题
+    field :fill_answer, type: Array    # 填空题
+    field :mapping_answer, type: Array # 连线题
 
     belongs_to :question, class_name: 'QuestionBank::Question'
-    belongs_to :user, class_name: 'QuestionBank::User'
+    belongs_to :user, class_name: 'User'
+
+    validates_each :bool_answer, :choice_answer, :essay_answer, :fill_answer, :mapping_answer do |record, attr, value|
+        record.errors.add attr, "attribute is wrong" 
+    end
+
 
 
   end
--- sample/spec/models/question_bank/question_record_spec.rb
+++ sample/spec/models/question_bank/question_record_spec.rb
@@ -2,16 +2,41 @@ require 'rails_helper'
 
 RSpec.describe QuestionBank::QuestionRecord, type: :model do
   describe "基础字段" do
+    # it{
+    #   @question_record = create(:question_record)
+    #   expect(@question_record.respond_to?(:question_id)).to eq(true)
+    #   expect(@question_record.respond_to?(:user_id)).to eq(true)
+    #   expect(@question_record.respond_to?(:is_correct)).to eq(true)
+    #   expect(@question_record.respond_to?(:bool_answer)).to eq(true)
+    #   expect(@question_record.respond_to?(:choice_answer)).to eq(true)
+    #   expect(@question_record.respond_to?(:essay_answer)).to eq(true)
+    #   expect(@question_record.respond_to?(:fill_answer)).to eq(true)
+    #   expect(@question_record.respond_to?(:mapping_answer)).to eq(true)
+    # }
+    before :each do 
+      @question_id    = "1234562789542136"
+      @user_id        = "2543215879654125"
+      @is_correct     = true
+      @bool_answer    = true
+      @choice_answer  = ["A","B","C"]
+      @essay_answer   = "good data"
+      @fill_answer    = [ "abc", "efg", "hij" ]
+      @mapping_answer = [ "A-a", "B-b", "C-c" ]
+
+      @question_record = QuestionBank::QuestionRecord.new(question_id: @question_id, user_id: @user_id, is_correct: @is_correct, bool_answer: @bool_answer, choice_answer: @choice_answer, essay_answer: @essay_answer, fill_answer: @fill_answer, mapping_answer: @mapping_answer)
+    end
+
     it{
-      @question_record = create(:question_record)
-      expect(@question_record.respond_to?(:question_id)).to eq(true)
-      expect(@question_record.respond_to?(:user_id)).to eq(true)
-      expect(@question_record.respond_to?(:is_correct)).to eq(true)
-      expect(@question_record.respond_to?(:bool_answer)).to eq(true)
-      expect(@question_record.respond_to?(:choice_answer)).to eq(true)
-      expect(@question_record.respond_to?(:essay_answer)).to eq(true)
-      expect(@question_record.respond_to?(:fill_answer)).to eq(true)
-      expect(@question_record.respond_to?(:mapping_answer)).to eq(true)
+      question = @question_record.clone
+      question.save
+      @question_record.valid?
+      # expect(@question_record).to be_valid
     }
+
+    it{
+      question = @question_record.clone
+      question.destroy
+      @question_record.valid?
+    } 
   end
 end
