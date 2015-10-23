6726f0e813b0b1f70d1f5c7c14502b06655f7bff
XAINI <shanzhenqiang0814@163.com>
2015-10-23 18:28:29 +0800

    update the question_bank model

--- app/models/question_bank/question_record.rb
+++ app/models/question_bank/question_record.rb
@@ -3,8 +3,8 @@ module QuestionBank
     include Mongoid::Document
     include Mongoid::Timestamps
 
-    field :question_id, type: String          # 题目的外键关联
-    field :user_id, type: String              #做题用户的外键关联
+    # field :question_id, type: String          # 题目的外键关联
+    # field :user_id, type: String              #做题用户的外键关联
     field :is_correct, type: Boolean          # 题目是否正确
 
     field :bool_answer, type: Boolean         # 判断题
@@ -14,51 +14,78 @@ module QuestionBank
     field :fill_answer, type: Array           # 填空题
     field :mapping_answer, type: Array        # 连线题
 
+    belongs_to :questions
+    belongs_to :user
+
     validate :validates_answer_kind
     validate :validates_answer_format
 
     # 校验类型
     def validates_answer_kind
-        @question_kind = QuestionBank::Question.where( self.question_id).kind
-        self.record_answer_ary.each do |r|
-            if r == @question_kind && r == nil
-                errors.add(:r, "#{r}不能为空")
+        question_kind = QuestionBank::Question.where(self.questions).kind
+        if question_kind == "bool_answer"
+            if bool_answer.blank?
+                errors.add(:bool_answer, "判断题不能为空")
             end
         end
 
-        self.record_answer_ary.each do |r|
-            if r != @question_kind && r != nil
-                errors.add(:r, "#{r}必须为空")
+        if question_kind == "essay_answer"
+            if essay_answer.blank?
+                errors.add(:essay_answer, "论述题不能为空")
             end
         end
+
+        if question_kind == "fill_answer"
+            if fill_answer.blank?
+                errors.add(:fill_answer, "填空题不能为空")
+            end
+        end
+
+        if question_kind == "mapping_answer"
+            if mapping_answer.blank?
+                errors.add(:mapping_answer, "连线题不能为空")
+            end
+        end
+
+        if question_kind == "choice_answer"
+            if choice_answer.blank?
+                errors.add(:choice_answer, "连线题不能为空")
+            end
+        end
+
+        # self.record_answer_ary.each do |r|
+        #     if r != question_kind && r != nil
+        #         errors.add(:r, "#{r}必须为空")
+        #     end
+        # end
     end
 
     # 校验格式
     def validates_answer_format
-        @question_kind = QuestionBank::Question.where( self.question_id).kind
-        if @question_kind == bool_answer && ( bool_answer != true || bool_answer != false )
-            errors.add(:bool_answer, "判断题答案格式不正确")
-        end
+    #     @question_kind = QuestionBank::Question.where( self.question_id).kind
+    #     if @question_kind == bool_answer && ( bool_answer != true || bool_answer != false )
+    #         errors.add(:bool_answer, "判断题答案格式不正确")
+    #     end
 
-        if @question_kind == single_choice_answer && single_choice_answer.count != 1
-            errors.add(:single_choice_answer, "单选题答案格式不正确")
-        end
+    #     if @question_kind == single_choice_answer && single_choice_answer.count != 1
+    #         errors.add(:single_choice_answer, "单选题答案格式不正确")
+    #     end
 
-        if @question_kind == multi_choice_answer && multi_choice_answer.count < 2
-            errors.add(:multi_choice_answer, "多选题答案格式不正确")
-        end
+    #     if @question_kind == multi_choice_answer && multi_choice_answer.count < 2
+    #         errors.add(:multi_choice_answer, "多选题答案格式不正确")
+    #     end
 
-        if @question_kind == essay_answer && !essay_answer.is_a?(String)
-            errors.add(:essay_answer, "论述题答案格式不正确")
-        end
+    #     if @question_kind == essay_answer && !essay_answer.is_a?(String)
+    #         errors.add(:essay_answer, "论述题答案格式不正确")
+    #     end
 
-        if @question_kind == fill_answer && ( @question_kind.count != fill_answer.count )
-            errors.add(:fill_answer, "填空题答案格式不正确")
-        end
+    #     if @question_kind == fill_answer && ( @question_kind.count != fill_answer.count )
+    #         errors.add(:fill_answer, "填空题答案格式不正确")
+    #     end
 
-         if @question_kind == mapping_answer && ( @question_kind.count != mapping_answer.count )
-            errors.add(:mapping_answer, "连线题答案格式不正确")
-        end
+    #      if @question_kind == mapping_answer && ( @question_kind.count != mapping_answer.count )
+    #         errors.add(:mapping_answer, "连线题答案格式不正确")
+    #     end
     end
 
     def question_id=(question_id)
