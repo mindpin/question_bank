81428e7f28fcc38547c5b93857372a21af8f17c1
libochao <522427432@qq.com>
2015-12-01 16:31:06 +0800

    完了

--- sample/spec/models/question_bank/question_flaw_spec.rb
+++ sample/spec/models/question_bank/question_flaw_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 RSpec.describe QuestionBank::QuestionFlaw, type: :model do
-  describe '测试方法' do
-    it{
-      user = create(:user)
-      bool_question = create(:bool_question)
-      before_add_count = user.flaw_questions.count
-      user.add_flaw_question(bool_question)
-      after_add_count = user.flaw_questions.count
-      expect(user.flaw_questions.first.question.content).to eq(bool_question.content)
+  describe 'UserMethods' do
+    before :all do
+      @user = create(:user)
+      @bool_question = create(:bool_question)
+    end
+
+    describe '测试方法add_flaw_question' do
+      it{
+      before_add_count = @user.flaw_questions.count
+      @user.add_flaw_question(@bool_question)
+      after_add_count = @user.flaw_questions.count
+      expect(@user.flaw_questions.first.question.content).to eq(@bool_question.content)
       expect(before_add_count+1).to eq(after_add_count)
-      expect(user.flaw_questions.count).to eq(1)
-      expect(QuestionBank::QuestionFlaw.where(:user => user,:question => bool_question).first.present?).to eq(true)
-      user.remove_flaw_question(bool_question)
-      expect(user.flaw_questions.count).to eq(0)
-      expect(QuestionBank::QuestionFlaw.where(:user => user,:question => bool_question).first.present?).to eq(false)
-    }
+      expect(@user.flaw_questions.count).to eq(1)
+      expect(QuestionBank::QuestionFlaw.where(:user => @user,:question => @bool_question).first.present?).to eq(true)
+      }
+    end
+
+    describe '测试方法remove_flaw_question' do
+      it{
+      @user.add_flaw_question(@bool_question)
+      @user.remove_flaw_question(@bool_question)
+      expect(@user.flaw_questions.count).to eq(0)
+      expect(QuestionBank::QuestionFlaw.where(:user => @user,:question => @bool_question).first.present?).to eq(false)
+      }
+    end
+
+    describe 'QuestionMethods::is_in_flaw_list_of?' do
+      it{
+        QuestionBank::QuestionFlaw.create(:user => @user,:question => @bool_question)
+        expect(@bool_question.is_in_flaw_list_of?(@user)).to eq(true)
+      }
+    end
   end
 end
