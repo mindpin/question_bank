b5fb5277ed9f7c1f0053fc06d9408ea810166680
libochao <522427432@qq.com>
2015-12-02 12:45:37 +0800

    done

--- app/models/question_bank/question_flaw.rb
+++ app/models/question_bank/question_flaw.rb
@@ -5,29 +5,34 @@ module QuestionBank
     belongs_to :question, :class_name => 'QuestionBank::Question'
     belongs_to :user, :class_name => QuestionBank.user_class
 
+    extend Enumerize
+    enumerize :kind, in: Question::KINDS
+    before_validation :set_kind
+    def set_kind
+        self.kind = self.question.kind
+    end
 
     module UserMethods
       extend ActiveSupport::Concern
         def flaw_questions
-          # TODO 需要重新实现
-          question_flaws
+          question_array = []
+          self.question_flaws.map do |flaw|
+            question_array.push(flaw.question)
+          end
+          # TODO 另一种方式
+          # question_array = self.question_flaws.only(:question_id,:kind).as_json
+          return QuestionBank::Question.where(:id.in => question_array)
         end
 
         def add_flaw_question(question)
-          return if question.question_flaws.where(:user_id => self.id.to_s).exists?
-
-          question.question_flaws.create(:user => self)
+          if !question.question_flaws.where(:user_id => self.id.to_s).exists?
+            question.question_flaws.create(:user => self)
+          end     
         end
 
         def remove_flaw_question(question)
           question.question_flaws.where(:user_id => self.id.to_s).destroy_all
         end
-    end
-
-    module QuestionMethods
-      def is_in_flaw_list_of?(user)
-        user.question_flaws.where(:question_id => self.id.to_s).exists?
-      end
     end
   end
 end
--- sample/spec/models/question_bank/question_flaw_spec.rb
+++ sample/spec/models/question_bank/question_flaw_spec.rb
@@ -8,13 +8,12 @@ RSpec.describe QuestionBank::QuestionFlaw, type: :model do
 
     describe '测试方法add_flaw_question' do
       it{
-      before_add_count = @user.flaw_questions.count
+      before_add_count = @user.question_flaws.count
       @user.add_flaw_question(@bool_question)
-      @user.add_flaw_question(@bool_question)
-      after_add_count = @user.flaw_questions.count
-      expect(@user.flaw_questions.first.question.content).to eq(@bool_question.content)
+      after_add_count = @user.question_flaws.count
+      expect(@user.question_flaws.first.question.content).to eq(@bool_question.content)
       expect(before_add_count+1).to eq(after_add_count)
-      expect(@user.flaw_questions.count).to eq(1)
+      expect(@user.question_flaws.count).to eq(1)
       expect(QuestionBank::QuestionFlaw.where(:user => @user,:question => @bool_question).first.present?).to eq(true)
       }
     end
@@ -22,7 +21,6 @@ RSpec.describe QuestionBank::QuestionFlaw, type: :model do
     describe '测试方法remove_flaw_question' do
       it{
       @user.add_flaw_question(@bool_question)
-      @user.remove_flaw_question(@bool_question)
       @user.remove_flaw_question(@bool_question)
       expect(@user.flaw_questions.count).to eq(0)
       expect(QuestionBank::QuestionFlaw.where(:user => @user,:question => @bool_question).first.present?).to eq(false)
@@ -33,6 +31,28 @@ RSpec.describe QuestionBank::QuestionFlaw, type: :model do
       it{
         QuestionBank::QuestionFlaw.create(:user => @user,:question => @bool_question)
         expect(@bool_question.is_in_flaw_list_of?(@user)).to eq(true)
+      }
+    end
+
+    # 测试冗余字段
+    describe '冗余字段' do
+      it{
+        flaw = QuestionBank::QuestionFlaw.create(:user => @user,:question => @bool_question)
+        expect(flaw.kind).to eq(@bool_question.kind)
+      }
+    end
+    # 测试方法
+    describe 'UserMethods::flaw_questions' do
+      it{
+        @bool_question = create(:bool_question)
+        @choice_question = create(:single_choice_question_wugui)
+        before_add_2_question_count = @user.flaw_questions.count
+        @user.add_flaw_question(@bool_question)
+        @user.add_flaw_question(@choice_question)
+        after_add_2_question_count = @user.flaw_questions.count
+        expect(@user.flaw_questions.where(:kind => "single_choice").first.content).to eq(@choice_question.content)
+        expect(@user.flaw_questions.where(:kind => "bool").first.content).to eq(@bool_question.content)
+        expect(before_add_2_question_count+2).to eq(after_add_2_question_count)
       }
     end
   end
