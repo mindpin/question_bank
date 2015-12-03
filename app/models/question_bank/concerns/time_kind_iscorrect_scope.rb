c138a415e41cfc8278e3ede418e03f2f683adb86
libochao <522427432@qq.com>
2015-12-03 12:17:48 +0800

    #done

--- app/models/question_bank/concerns/time_kind_iscorrect_scope.rb
+++ app/models/question_bank/concerns/time_kind_iscorrect_scope.rb
@@ -2,25 +2,25 @@ module QuestionBank
   module TimeKindIscorrectScope
     extend ActiveSupport::Concern
     included do
-    scope :with_created_at, -> (start_time,end_time) {
-      if (!start_time.nil? && !end_time.nil?) || (start_time.nil? && end_time.nil?)
-        where(:created_at.gte => start_time, :created_at.lte => end_time)
-      end
-      if start_time.nil?
-        where(:created_at.lte => end_time)
-      end 
-      if end_time.nil?
-        where(:created_at.gte => start_time)
-      end
-    }
+      scope :with_created_at, -> (start_time,end_time) {
+        if (!start_time.nil? && !end_time.nil?) || (start_time.nil? && end_time.nil?)
+          where(:created_at.gte => start_time, :created_at.lte => end_time)
+        end
+        if start_time.nil?
+          where(:created_at.lte => end_time)
+        end 
+        if end_time.nil?
+          where(:created_at.gte => start_time)
+        end
+      }
 
-    scope :with_kind, -> (kind) {
-      where(:kind => kind)
-    }
+      scope :with_kind, -> (kind) {
+        where(:kind => kind)
+      }
 
-    scope :with_correct, -> (is_correct) {
-      where(:is_correct => is_correct)
-    }
-  end
+      scope :with_correct, -> (is_correct) {
+        where(:is_correct => is_correct)
+      }
+    end
   end
 end
