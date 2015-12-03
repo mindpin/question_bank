e7cd89e1727b72a4fb0c3ffe2b22e889c5542acf
XAINI <shanzhenqiang0814@163.com>
2015-12-03 12:13:25 +0800

    updated record and flaw test,it will insert  timecop

--- sample/spec/models/question_bank/question_flaw_spec.rb
+++ sample/spec/models/question_bank/question_flaw_spec.rb
@@ -72,24 +72,19 @@ RSpec.describe QuestionBank::QuestionFlaw, type: :model do
 
     describe "成功" do
       it{
-        @start_time = "2015-12-01"
-        @end_time = Time.now
-        @batch_search = QuestionBank::QuestionFlaw.with_created_at(@start_time.to_time, @end_time.to_time).to_a
-        expect(@batch_search).to eq(@flaw.to_a)
-      }
-
-      it{
-        @start_time = "2015-12-02"
-        @end_time = nil
-        @batch_search = QuestionBank::QuestionFlaw.with_created_at(@start_time.to_time, @end_time).to_a
-        expect(@batch_search).to eq(@flaw.to_a)
-      }
-
-      it{
-        @start_time = nil
-        @end_time = "2015-12-02"
-        @batch_search = QuestionBank::QuestionFlaw.with_created_at(@start_time, @end_time).to_a
-        expect(@batch_search).to eq(@flaw.to_a)
+        @day_1 = Timecop.freeze(Time.local(2015, 12, 01, 10, 0, 0))
+        @day_2 = Timecop.freeze(Time.local(2015, 12, 02, 10, 0, 0))
+        @day_3 = Timecop.freeze(Time.local(2015, 12, 06, 10, 0, 0))
+        @day_4 = nil
+        times_combination = [
+          [@day_1,@day_2],
+          [@day_2,@day_4],
+          [@day_4,@day_2]
+        ]
+        times_combination.each do |arr|
+          @batch_search = QuestionBank::QuestionFlaw.with_created_at(arr[0], arr[1]).to_a
+          expect(@batch_search).to eq(@flaw.to_a)
+        end
       }
     end
 
