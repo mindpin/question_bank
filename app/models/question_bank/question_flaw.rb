f2c9559877c3fd29ce0eaab1689f5580fde38d5c
XAINI <shanzhenqiang0814@163.com>
2015-12-04 09:23:34 +0800

    About test record and flaw code

--- app/models/question_bank/question_flaw.rb
+++ app/models/question_bank/question_flaw.rb
@@ -14,13 +14,13 @@ module QuestionBank
 
     scope :with_created_at, -> (start_time,end_time) {
       if (!start_time.nil? && !end_time.nil?) || (start_time.nil? && end_time.nil?)
-        where(:created_at.gte => start_time, :created_at.lte => end_time)
+        return where(:created_at.gte => start_time, :created_at.lte => end_time)
       end
       if start_time.nil?
-        where(:created_at.lte => end_time)
+        return where(:created_at.lte => end_time)
       end 
       if end_time.nil?
-        where(:created_at.gte => start_time)
+        return where(:created_at.gte => start_time)
       end
     }
 
