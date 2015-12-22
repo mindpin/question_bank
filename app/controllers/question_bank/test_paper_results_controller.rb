d0f1b9170d1e64a0ff6cb0076767cef080b6675b
fushang318 <fushang318@gmail.com>
2015-12-22 12:14:29 +0800

    简化

--- app/controllers/question_bank/test_paper_results_controller.rb
+++ app/controllers/question_bank/test_paper_results_controller.rb
@@ -6,18 +6,13 @@ module QuestionBank
     end
 
     def create
+      params.permit!
       test_paper = TestPaper.find(params[:test_paper_id])
-      test_paper_result = test_paper.test_paper_results.new(:question_records_attributes => params[:test_paper_result][:question_records_attributes],:user => current_user)
-      if test_paper_result.save
-        render :json => {
-          :test_paper_results_msg => test_paper_result.get_test_paper_results_msg 
-        }
-      end
+      test_paper_result = test_paper.test_paper_results.new(:question_records_attributes => params[:test_paper_result][:question_records_attributes])
+      test_paper_result.user = current_user
+      test_paper_result.save
+      render :json => test_paper_result.to_create_hash
     end
 
-    private
-      # def test_paper_result_params
-      #   params.require(:test_paper_result).permit(:question_records_attributes => {:question_id=>[],:user_id=>[],:answer=>[]})
-      # end
   end
 end
