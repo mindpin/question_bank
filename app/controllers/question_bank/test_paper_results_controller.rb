d59e61f5dbd606454280b06560e481d592197a71
libochao <522427432@qq.com>
2015-12-22 10:41:34 +0800

    结果生成

--- app/controllers/question_bank/test_paper_results_controller.rb
+++ app/controllers/question_bank/test_paper_results_controller.rb
@@ -4,5 +4,20 @@ module QuestionBank
       @test_paper = TestPaper.find(params[:test_paper_id])
       @test_paper_result = @test_paper.test_paper_results.new
     end
+
+    def create
+      test_paper = TestPaper.find(params[:test_paper_id])
+      test_paper_result = test_paper.test_paper_results.new(:question_records_attributes => params[:test_paper_result][:question_records_attributes],:user => current_user)
+      if test_paper_result.save
+        render :json => {
+          :test_paper_results_msg => test_paper_result.get_test_paper_results_msg 
+        }
+      end
+    end
+
+    private
+      # def test_paper_result_params
+      #   params.require(:test_paper_result).permit(:question_records_attributes => {:question_id=>[],:user_id=>[],:answer=>[]})
+      # end
   end
 end
--- app/models/question_bank/test_paper_result.rb
+++ app/models/question_bank/test_paper_result.rb
@@ -7,5 +7,28 @@ module QuestionBank
     belongs_to :test_paper, :class_name => "QuestionBank::TestPaper"
     has_and_belongs_to_many :question_records, class_name:'QuestionBank::QuestionRecord'
     accepts_nested_attributes_for :question_records, allow_destroy: true
+
+    def make_test_paper_results_msg(id,result,answer)
+      @results_msg["#{id}"] = [:is_correct => result,:answer => answer]
+    end
+
+    def get_test_paper_results_msg 
+      return @results_msg
+    end
+
+    def question_records_attributes=(str)
+      @results_msg = {}
+      question_record_list =str.map do |key,value|
+        value
+      end
+      question_record_list.each do |record_params|
+        question = Question.find(record_params[:question_id])
+        question_record = question.question_records.create(
+          :user          => User.find(record_params[:user_id]),
+          :answer        => record_params[:answer]
+        )
+        make_test_paper_results_msg(record_params[:question_id],question_record.is_correct,question_record.correct_answer)
+      end
+    end
   end
 end
