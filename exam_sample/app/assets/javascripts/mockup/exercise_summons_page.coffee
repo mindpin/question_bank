ad9751b7185f39cc1546fe92e3490f713a568811
destinyd <destinyd_war@163.com>
2016-05-16 15:05:53 +0800

    翻打传票练习

---
+++ exam_sample/app/assets/javascripts/mockup/exercise_summons_page.coffee
@@ -0,0 +1,33 @@
+@ExerciseSummonsPage = React.createClass
+  next: (answer)->
+    right = @state.question_right
+    right += 1 if @state.question.answer == answer
+    @setState
+      question_index: @state.question_index + 1
+      question: @props.data.questions[@state.question_index + 1]
+      question_right: right
+
+  getInitialState: ->
+    question_index: 0
+    question_right: 0
+    question: @props.data.questions[0]
+
+  render: ->
+    params =
+      data: @props.data
+      question: @state.question
+      next: @next
+      question_index: @state.question_index
+      question_right: @state.question_right
+
+    <ExerciseBase {...params}>
+      <div>
+        {
+          @state.question.content
+        }
+      </div>
+
+      <br />
+
+    </ExerciseBase>
+
--- exam_sample/app/controllers/exercises_controller.rb
+++ exam_sample/app/controllers/exercises_controller.rb
@@ -3,15 +3,32 @@ class ExercisesController < ApplicationController
   def index
     if DemoQuestion.subjects.include?(params[:subject])
       @page_name = "exercise_#{params[:subject]}"
+      subject = params[:subject]
       @component_data = {
-        questions: (1..10).map do |i|
-          num = rand(56887746371819585459)
-          {
-            content: num.to_s,
-            answer: num.to_s
-          }
-        end
+        subject: subject
       }
+      case subject
+      when 'number'
+        @component_data[:questions] = 
+          (1..10).map do |i|
+            num = rand(56887746371819585459)
+            {
+              content: num.to_s,
+              answer: num.to_s
+            }
+          end
+      when 'summons'
+        format = "%0.2f"
+        @component_data[:questions] =
+          (1..10).map do |i|
+            num = format % (rand * 103326588836)
+            {
+              content: num.to_s,
+              answer: num.to_s
+            }
+          end
+      else
+      end
 
 
       render "mockup/page"
